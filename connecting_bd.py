90aa9cc3a2c249d2614257063b76305bcc97c979
Диана <ravilova.dm@phystech.edu>
2022-12-05 20:01:57 +0300

    конец

--- connecting_bd.py
+++ connecting_bd.py
@@ -4,34 +4,15 @@ class film_data(object):
     def __init__(self):
         self._engine=create_engine("sqlite:///films.db", echo = True)
 
-    def get_actors(self, film_id):
-        sql = text("SELECT name from person as a \
-join film_actor as c on c.person_id = a.person_id \
+    def get_persons(self, film_id):
+        sql = text("SELECT a.person_id as person_id, a.name as name, d.name as post from person as a \
+join film_person as c on c.person_id = a.person_id \
+join jobs as d on c.post_id = d.post_id \
 where c.film_id = " + str (film_id) + ";")
         sql_result = self._engine.execute(sql)
         ret = []
         for record in sql_result:
-            ret.append((record[0]))
-        return ret
-
-    def get_directors(self, film_id):
-        sql = text("SELECT a.name from person as a \
-join film_director as c on c.person_id = a.person_id \
-where c.film_id = " + str (film_id) + ";")
-        sql_result = self._engine.execute(sql)
-        ret = []
-        for record in sql_result:
-            ret.append((record[0]))
-        return ret
-
-    def get_scriptwriters(self, film_id):
-        sql = text("SELECT a.name from person as a \
-join film_scriptwriter as c on c.person_id = a.person_id \
-where c.film_id = " + str (film_id) + ";")
-        sql_result = self._engine.execute(sql)
-        ret = []
-        for record in sql_result:
-            ret.append((record[0]))
+            ret.append(dict(record))
         return ret
 
     def get_country_of_film(self, film_id):
@@ -54,7 +35,7 @@ where c.film_id = " + str (film_id) + ";")
             ret.append((record[0]))
         return ret
     def get_all_films(self):
-        sql = text("SELECT film_id ,name from film order by name")
+        sql = text("SELECT film_id,name from film order by name")
         sql_result = self._engine.execute(sql)
         ret = []
         for record in sql_result:
@@ -80,17 +61,10 @@ where c.person_id = " + str (person_id) + ";")
         return ret
 
     def get_persons_films(self, person_id):
-        sql = text("SELECT a.film_id, a.name, 'actor' as post from film as a \
-join film_actor as b on b.film_id = a.film_id \
-where b.person_id = " + str(person_id) + 
-" UNION \
-SELECT a.film_id, a.name, 'director' as post from film as a \
-join film_director as b on b.film_id = a.film_id \
-where b.person_id = " + str(person_id) + 
-" UNION \
-SELECT a.film_id, a.name, 'scriptwriter' as post from film as a \
-join film_scriptwriter as b on b.film_id = a.film_id \
-where b.person_id = " + str(person_id) + ";")
+        sql = text("SELECT a.film_id as film_id, b.name as name, c.name as post_name from film_person as a \
+join film as b on b.film_id = a.film_id \
+join jobs as c on c.post_id = a.post_id \
+where a.person_id = " + str(person_id) + ";")
         sql_result = self._engine.execute(sql)
         ret = []
         for record in sql_result:
@@ -98,16 +72,16 @@ where b.person_id = " + str(person_id) + ";")
         return ret
 
     def get_persons_life(self, person_id):
-        sql = text("SELECT a.person_id, name, a.birth_year as birth, b.year as death \
+        sql = text("SELECT a.person_id as person_id, name, a.birth_year as birth, b.year as death \
              from person as a left join death as b on b.person_id = a.person_id \
             where a.person_id = " + str(person_id) + ";")
         sql_result = self._engine.execute(sql)
         ret = []
         for record in sql_result:
             ret.append(dict(record))
-        return ret
+        return ret[0]
     
-    def get_person_name(person_id):
+    def get_person_name(self, person_id):
         sql = text("SELECT name from person where person_id = " + str(person_id) + ";")
         sql_result = self._engine.execute(sql)
         ret = []
@@ -115,8 +89,25 @@ where b.person_id = " + str(person_id) + ";")
             ret.append((record[0]))
         return ret
     
+    def get_film_name(self, film_id):
+        sql = text("SELECT name from film where film_id = " + str(film_id) + ";")
+        sql_result = self._engine.execute(sql)
+        ret = []
+        for record in sql_result:
+            ret.append((record[0]))
+        return ret
+
+    def get_film_date(self, film_id):
+        sql = text("SELECT year from film where film_id = " + str(film_id) + ";")
+        sql_result = self._engine.execute(sql)
+        ret = []
+        for record in sql_result:
+            ret.append((record[0]))
+        return ret
+
 bd = film_data()
-print (bd.get_persons_films(1))
+print (bd.get_persons_life(1))
+
 
 
 
