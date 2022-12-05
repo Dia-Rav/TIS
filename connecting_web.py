88831b09eca151a37aa4d70e5f4285533ffe6335
Диана <ravilova.dm@phystech.edu>
2022-12-05 20:01:48 +0300

    конец проекта

--- connecting_web.py
+++ connecting_web.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from connecting_db import film_data
+from connecting_bd import film_data
 
 app = Flask(__name__)
 bd = film_data()
@@ -28,15 +28,20 @@ def films():
     ret = render_template("films.html",films_list=films_list)
     return ret
 
-@app.route("/person/<person_id>")
-def author(author_id=None):
-    """Обрабатывает запрос к странице конретного автора http://mipt-space-tis.ru:5015/person """
-    person_name = bd.get_person_name(person_id)
-    countries = bd.get_country_of_person(person_id)
-    job = bd.get_persons_films(person_id)
-    ret = render_template("person.html", person_name = person_name, countries = countries, films = job)
-#ret ="<pre>" + str(author_name) +"\n" + str(countries) + "\n" + str(books) + "</pre>"
+@app.route("/")
+def start():
+    ret = render_template("start.html")
     return ret
 
-app.run(host='0.0.0.0',port=5015)
 
+@app.route("/film/<film_id>")
+def film(film_id=None):
+    film_name = bd.get_film_name(film_id)
+    film_date = bd.get_film_date(film_id)
+    genres = bd.get_genre(film_id)
+    countries = bd.get_country_of_film(film_id)
+    person_list = bd.get_persons(film_id)
+    ret = render_template("film.html", countries = countries, year = film_date, 
+    person_list = person_list, genres = genres)
+    return ret
+app.run(host='0.0.0.0',port=5015)
---
+++ creating_bd.sql
@@ -0,0 +1,88 @@
+-- SQLite
+
+PRAGMA foreign_keys=on;
+PRAGMA encoding="UTF-8";
+
+
+CREATE TABLE film (film_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, year INTEGER);
+CREATE TABLE person (person_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT Not NULL, birth_year INTEGER);
+CREATE TABLE jobs (post_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);
+CREATE TABLE film_person (person_id INTEGER NOT NULL, film_id INTEGER NOT NULL, post_id INTEGER NOT NULL, PRIMARY KEY (person_id, film_id, post_id), FOREIGN KEY (person_id) REFERENCES person(person_id), FOREIGN KEY (film_id) REFERENCES film(film_id), FOREIGN KEY (post_id) REFERENCES jobs(post_id));
+CREATE TABLE person_country ( person_id INTEGER NOT NULL, country_id  INTEGER NOT NULL, PRIMARY KEY (country_id, person_id), FOREIGN KEY (person_id) REFERENCES person(person_id), FOREIGN KEY (country_id) REFERENCES country(id));
+CREATE TABLE country (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);
+CREATE TABLE death (person_id INTEGER NOT NULL PRIMARY KEY, year INTEGER, FOREIGN KEY(person_id) REFERENCES person(person_id));
+CREATE TABLE film_country ( film_id INTEGER NOT NULL, country_id  INTEGER NOT NULL, PRIMARY KEY (country_id, film_id), FOREIGN KEY (film_id) REFERENCES film(film_id), FOREIGN KEY (country_id) REFERENCES country(id));
+CREATE TABLE genre (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);
+CREATE TABLE film_genre ( film_id INTEGER NOT NULL, genre_id  INTEGER NOT NULL, PRIMARY KEY (genre_id, film_id), FOREIGN KEY (film_id) REFERENCES film(film_id), FOREIGN KEY (genre_id) REFERENCES genre(id));
+
+
+INSERT INTO film VALUES (NULL, "Гарри Поттер и философский камень",  2001),
+(NULL, "Век Адалин", 2015),
+(NULL, "Убийство в восточном экспрессе", 2017);
+
+INSERT INTO person VALUES 
+(NULL, "Дэниэл Рэдклифф", 1989),
+(NULL, "Руперт Гринт", 1988), 
+(NULL, "Эмма Уотсон", 1990),
+(NULL, "Алан Рикман", 1946), 
+(NULL, "Крис Коламбус", 1958), 
+(NULL, "Стивен Кловз", 1960), 
+(NULL, "Дж.К. Роулинг", 1965), 
+(NULL, "Ли Толанд Кригер", 1983), 
+(NULL, "Дж. Миллс Гудло", 1961), 
+(NULL, "Блейк Лайвли", 1988), 
+(NULL, "Михил Хёйсман", 1981), 
+(NULL, "Кеннет Брана", 1960), 
+(NULL, "Агата Кристи", 1890), 
+(NULL, "Пенелопа Крус", 1984), 
+(NULL, "Уиллем Дефо", 1955);
+
+INSERT INTO death Values (4,2016);
+
+INSERT INTO country VALUES (NULL,"Российская Федерация"), 
+(NULL,"Великобритания"),
+(NULL,"США"), 
+(NULL,"Канада"), 
+(NULL,"Мальта"),
+(NULL, "Франция"),
+(NULL, "Нидерланды"),
+(NULL, "Испания");
+
+INSERT INTO genre VALUES (NULL,"фэнтези"), 
+(NULL,"приключения"), 
+(NULL,"семейный"), 
+(NULL,"драма"), 
+(NULL,"мелодрама"), 
+(NULL,"детектив"), 
+(NULL,"криминал"), 
+(NULL,"триллер"), 
+(NULL,"ужасы");
+
+INSERT INTO jobs VALUES (NULL, "актер"), (NULL, "режиссер"), (NULL, "сценарист");
+
+INSERT INTO film_person VALUES (4, 1, 1), (3, 1, 1), (2, 1, 1), (1, 1, 1), (10, 2, 1), (11, 2, 1), (12, 3, 1), (14, 3, 1),
+(6, 1, 3), (7, 1, 3), (9, 2, 3), (13, 3, 3), (5, 1, 2), (8, 2, 2), (12, 3, 2);
+
+INSERT INTO film_country VALUES (1, 2), (1, 3), (3, 2), (2, 4), (2, 3), (3, 3), (3, 5);
+
+INSERT INTO person_country VALUES 
+(1, 2),
+(2, 2),
+(3 , 6),
+(4, 2),
+( 5, 3),
+( 6, 3),
+(7 , 2),
+( 8, 3),
+( 9, 3),
+( 10, 3),
+( 11, 7),
+( 12, 2),
+( 13, 2),
+( 14, 8),
+( 15, 3);
+
+INSERT INTO film_genre VALUES 
+(1, 1), (1, 2), (1, 3), (2, 1) , (2, 4), (2, 5), (3, 6), (3, 7), (3, 4);
+
+INSERT INTO person_country (
