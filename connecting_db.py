be14eccd5f2f0de07292fd381055dfec8a60dfe1
Диана <ravilova.dm@phystech.edu>
2022-12-05 11:15:34 +0300

    connectig web + html

--- connecting_db.py
+++ connecting_db.py
@@ -60,6 +60,7 @@ where c.film_id = " + str (film_id) + ";")
         for record in sql_result:
             ret.append(dict(record))
         return ret
+
     def get_all_persons(self):
         sql = text("SELECT person_id, name from person order by name")
         sql_result = self._engine.execute(sql)
@@ -67,6 +68,7 @@ where c.film_id = " + str (film_id) + ";")
         for record in sql_result:
             ret.append(dict(record))
         return ret
+
     def get_country_of_person(self, person_id):
         sql = text("SELECT a.name from country as a \
 join person_country as c on c.country_id = a.id \
@@ -76,6 +78,7 @@ where c.person_id = " + str (person_id) + ";")
         for record in sql_result:
             ret.append((record[0]))
         return ret
+
     def get_persons_films(self, person_id):
         sql = text("SELECT a.film_id, a.name, 'actor' as post from film as a \
 join film_actor as b on b.film_id = a.film_id \
@@ -104,8 +107,16 @@ where b.person_id = " + str(person_id) + ";")
             ret.append(dict(record))
         return ret
     
+    def get_person_name(person_id):
+        sql = text("SELECT name from person where person_id = " + str(person_id) + ";")
+        sql_result = self._engine.execute(sql)
+        ret = []
+        for record in sql_result:
+            ret.append((record[0]))
+        return ret
+    
 bd = film_data()
-print (bd.get_all_films())
+print (bd.get_persons_films(1))
 
 
 
---
+++ connecting_web.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from connecting_db import film_data
+
+app = Flask(__name__)
+bd = film_data()
+
+@app.route("/persons")
+def persons():
+    """Обрабатывает запрос к странице со списком авторов http://mipt-space-tis.ru:5015/ """
+    persons_list = bd.get_all_persons()
+    ret = render_template("persons.html",persons_list=persons_list)
+    return ret
+
+@app.route("/person/<person_id>")
+def person(person_id=None):
+    """Обрабатывает запрос к странице конретного автора http://mipt-space-tis.ru:5015/person """
+    person_name = bd.get_person_name(person_id)
+    countries = bd.get_country_of_person(person_id)
+    job = bd.get_persons_films(person_id)
+    life = bd.get_persons_life(person_id)
+    ret = render_template("person.html", person_name = person_name, countries = countries, films = job, life = life)
+#ret ="<pre>" + str(author_name) +"\n" + str(countries) + "\n" + str(books) + "</pre>"
+    return ret
+
+@app.route("/films")
+def films():
+    films_list = bd.get_all_films()
+    ret = render_template("films.html",films_list=films_list)
+    return ret
+
+@app.route("/person/<person_id>")
+def author(author_id=None):
+    """Обрабатывает запрос к странице конретного автора http://mipt-space-tis.ru:5015/person """
+    person_name = bd.get_person_name(person_id)
+    countries = bd.get_country_of_person(person_id)
+    job = bd.get_persons_films(person_id)
+    ret = render_template("person.html", person_name = person_name, countries = countries, films = job)
+#ret ="<pre>" + str(author_name) +"\n" + str(countries) + "\n" + str(books) + "</pre>"
+    return ret
+
+app.run(host='0.0.0.0',port=5015)
+
